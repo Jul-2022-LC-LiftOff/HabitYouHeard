c68b35f1c2af6f068b2459fbb21a2326c6e571be
Micah <micahnyoung95@gmail.com>
2022-07-28 19:55:56 -0500

    first commit

---
+++ src/main/java/com/habityouheard/habityouheard/HabityouheardApplication.java
@@ -0,0 +1,13 @@
+package com.habityouheard.habityouheard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabityouheardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabityouheardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habityouheard/habityouheard/HabityouheardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habityouheard.habityouheard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabityouheardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
