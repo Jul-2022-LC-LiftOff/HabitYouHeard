d473c7bba00b2411559a01c9dc0f0637ef3f3409
Eric <erikkustrife@gmail.com>
2022-08-11 19:45:58 -0500

    UserController Eric Smith changed

--- src/main/java/com/habityouheard/habityouheard/Controllers/UserController.java
+++ src/main/java/com/habityouheard/habityouheard/Controllers/UserController.java
@@ -5,12 +5,12 @@ import com.habityouheard.habityouheard.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 ///api/user
-@Controller
+@CrossOrigin
+@RestController
 @RequestMapping("api/user")
 public class UserController {
 
