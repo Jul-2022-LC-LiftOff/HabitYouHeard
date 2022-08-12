01f41c03065e7205469a1a743aa2696f1d575ec5
Brandon Cope <hunterv96@gmail.com>
2022-08-11 19:41:34 -0500

    HabitsController is operable!

--- src/main/java/com/habityouheard/habityouheard/controllers/HabitsController.java
+++ src/main/java/com/habityouheard/habityouheard/controllers/HabitsController.java
@@ -4,22 +4,25 @@ import com.habityouheard.habityouheard.models.Habit;
 import com.habityouheard.habityouheard.models.User;
 import com.habityouheard.habityouheard.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RequestMapping("api")
+@RestController
+@RequestMapping("api/habits")
 public class HabitsController {
 
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("habits/{userId}")
+    @GetMapping("{userId}")
     public List<Habit> returnUserHabits(@PathVariable int userId){
         Optional<User> optUser = userRepository.findById(userId);
         if(optUser.isPresent()) { //Thought: what happens when the user doesn't have any habits? Does the list just remain empty?
