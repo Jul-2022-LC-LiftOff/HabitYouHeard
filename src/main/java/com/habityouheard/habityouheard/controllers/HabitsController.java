41116fd311d10830f376372bc0e132b2f9fdd9d9
Brandon Cope <hunterv96@gmail.com>
2022-08-13 10:37:05 -0500

    Refactored {id} route to return a ResponseEntity

--- src/main/java/com/habityouheard/habityouheard/controllers/HabitsController.java
+++ src/main/java/com/habityouheard/habityouheard/controllers/HabitsController.java
@@ -1,9 +1,11 @@
 package com.habityouheard.habityouheard.controllers;
 
 import com.habityouheard.habityouheard.models.Habit;
+import com.habityouheard.habityouheard.models.HabitMeta;
 import com.habityouheard.habityouheard.models.User;
 import com.habityouheard.habityouheard.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,17 +22,14 @@ public class HabitsController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("{userId}")
-    public List<Habit> returnUserHabits(@PathVariable int userId){
-        Optional<User> optUser = userRepository.findById(userId);
-        if(optUser.isPresent()) { //Thought: what happens when the user doesn't have any habits? Does the list just remain empty?
+    @GetMapping("{id}")
+    public ResponseEntity<List<Habit>> returnUserHabits(@PathVariable int id){
+        Optional <User> optUser = userRepository.findById(id);
+        if(optUser.isPresent()){
             User user = (User) optUser.get();
             List<Habit> habits = user.getHabits();
-            return habits;
-        } else {
-            //do we want to have a variable that contains whether the habit list has any habits? Perhaps it replaces the list with
-                                                                             //some text like "You do not have any habits, would you like to make one?", and then has a hyperlink to the create page.
-            return new ArrayList<Habit>();
+            return ResponseEntity.ok().body(habits);
         }
+        return ResponseEntity.ok().body(new ArrayList<>());
     }
 }
